288776a725f3062cee6d8d9309052f355c938129
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-20 15:50:00 -0500

    adding some tool changes.

--- src/app/_components/chat.tsx
+++ src/app/_components/chat.tsx
@@ -9,6 +9,13 @@ export function ChatDemo(props: { id: string; initialMessages: Message[] }) {
 		id: props.id,
 		initialMessages: props.initialMessages,
 		sendExtraMessageFields: true,
+		maxSteps: 5,
+		async onToolCall({ toolCall }) {
+			if (toolCall.toolName === "getLocation") {
+				const cities = ["New York", "Los Angeles", "Chicago", "San Francisco"];
+				return cities[Math.floor(Math.random() * cities.length)];
+			}
+		},
 	});
 
 	return (
--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { openai } from '@ai-sdk/openai';
 import { streamText, type Message } from 'ai';
+import { z } from 'zod';
 import { api } from '~/trpc/server';
 
 export const maxDuration = 30;
@@ -9,6 +10,32 @@ export async function POST(req: Request) {
   const result = streamText({
     model: openai('gpt-4o'),
     messages,
+    tools: {
+      // server-side tool with execute function:
+      getWeatherInformation: {
+        description: 'show the weather in a given city to the user',
+        parameters: z.object({ city: z.string() }),
+        execute: async ({ city }: { city: string }) => {
+          const weatherOptions = ['sunny', 'cloudy', 'rainy', 'snowy', 'windy'];
+          return weatherOptions[
+            Math.floor(Math.random() * weatherOptions.length)
+          ];
+        },
+      },
+      // client-side tool that starts user interaction:
+      askForConfirmation: {
+        description: 'Ask the user for confirmation.',
+        parameters: z.object({
+          message: z.string().describe('The message to ask for confirmation.'),
+        }),
+      },
+      // client-side tool that is automatically executed on the client:
+      getLocation: {
+        description:
+          'Get the user location. Always ask for confirmation before using this tool.',
+        parameters: z.object({}),
+      },
+    },
     onError: async (error) => {
       console.error(error);
     },
